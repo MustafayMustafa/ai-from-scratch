a722e11d863c89e1c7727d853cfcb7fc03862f39
Mustafa <mm17573@essex.ac.uk>
2024-10-07 00:56:26 +0100

    tanh and test

--- tests/test_activation_functions.py
+++ tests/test_activation_functions.py
@@ -1,5 +1,7 @@
 import pytest
-from utils.activation_functions import sigmoid, relu, softmax
+import numpy as np
+import numpy.testing as npt
+from utils.activation_functions import sigmoid, relu, softmax, tanh
 
 
 def test_sigmoid():
@@ -14,35 +16,62 @@ def test_sigmoid():
 
 
 def test_relu():
-    assert relu([1, 0, -1]) == [1, 0, 0]
+    input_data = np.array([1, 0, -1])
+    expected_output = np.array([1, 0, 0])
+    npt.assert_array_equal(relu(input_data), expected_output)
 
 
 @pytest.fixture
-def basic_case():
-    return [2.0, 1.0, 0.1], [0.65900114, 0.24243297, 0.09856589]
+def soft_max_positive_values():
+    return np.array([2.0, 1.0, 0.1]), np.array([0.65900114, 0.24243297, 0.09856589])
 
 
 @pytest.fixture
-def large_values_case():
-    return [1000, 1000, 1000], [1 / 3, 1 / 3, 1 / 3]
+def soft_max_negative_values():
+    return np.array([-2.0, -1.0, -0.1]), np.array([0.09611525, 0.26126834, 0.64261641])
 
 
 @pytest.fixture
-def negative_values_case():
-    return [-1, -2, -3], [0.66524096, 0.24472847, 0.09003057]
+def soft_max_large_values():
+    return np.array([1000, 1000, 1000]), np.array([1 / 3, 1 / 3, 1 / 3])
 
 
 @pytest.fixture
-def zero_values_case():
-    return [0.0, 0.0, 0.0], [1 / 3, 1 / 3, 1 / 3]
+def soft_max_zero_values():
+    return np.array([0.0, 0.0, 0.0]), np.array([1 / 3, 1 / 3, 1 / 3])
 
 
-def test_softmax_basic(basic_case):
-    input_data, expected_output = basic_case
-    softmax_result = softmax(input_data)
-    assert all(
-        [
-            pytest.approx(result, rel=1e-6) == expected
-            for result, expected in zip(softmax_result, expected_output)
-        ]
-    )
+def test_softmax_positive(soft_max_positive_values):
+    input_data, expected_output = soft_max_positive_values
+    result = softmax(input_data)
+    npt.assert_allclose(result, expected_output, rtol=1e-6)
+
+
+def test_softmax_negative(soft_max_negative_values):
+    input_data, expected_output = soft_max_negative_values
+    result = softmax(input_data)
+    npt.assert_allclose(result, expected_output, rtol=1e-6)
+
+
+def test_softmax_zero(soft_max_zero_values):
+    input_data, expected_output = soft_max_zero_values
+    result = softmax(input_data)
+    npt.assert_allclose(result, expected_output, rtol=1e-6)
+
+
+def test_softmax_large(soft_max_large_values):
+    input_data, expected_output = soft_max_large_values
+    result = softmax(input_data)
+    npt.assert_allclose(result, expected_output, rtol=1e-6)
+
+
+def test_tanh():
+    assert tanh(0) == 0
+
+    # Test for small positive and negative values
+    assert tanh(1) == pytest.approx(np.tanh(1), rel=1e-6)
+    assert tanh(-1) == pytest.approx(np.tanh(-1), rel=1e-6)
+
+    # Test for large positive and negative values
+    assert tanh(1000) == pytest.approx(np.tanh(1000), rel=1e-6)
+    assert tanh(-1000) == pytest.approx(np.tanh(-1000), rel=1e-6)
--- utils/activation_functions.py
+++ utils/activation_functions.py
@@ -1,18 +1,32 @@
-import math
+import numpy as np
 
 
-def sigmoid(value):
-    # two formulations to handle numerical stabillity for both large positive and negative values
-    if value > 0:
-        return 1 / (1 + math.pow(math.e, -value))
-    else:
-        return math.pow(math.e, value) / (1 + math.pow(math.e, value))
+def sigmoid(x):
+    # clamp for numerical stability
+    x = np.clip(x, -700, 700)
+
+    # two formulations for numerical stability
+    return np.where(
+        x >= 0,
+        1 / (1 + np.exp(-x)),
+        np.exp(x) / (1 + np.exp(x)),
+    )
 
 
 def relu(x):
-    return [max(0, value) for value in x]
+    return np.maximum(0, x)
 
 
 def softmax(x):
-    exponentials = [math.exp(value) for value in x]
-    return [exp / sum(exponentials) for exp in exponentials]
+    # subtract max value for numerical stability since it is invariant to constant shifts in the input
+    exponentials = np.exp(x - np.max(x))
+    return exponentials / np.sum(exponentials)
+
+
+def tanh(x):
+    # large values saturate to +-1 so can handle numerical stability like so
+    return np.where(
+        x > 20,
+        1,
+        np.where(x < -20, -1, (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))),
+    )
